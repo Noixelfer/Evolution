fdce55c12f33469d1bbd34aecbcf0e647849d74b
Noixelfer <pauldarius98@gmail.com>
2019-04-02 10:52:55 +0300

    Create Game script (singleton)

---
+++ Evolution/Assets/Scripts/Core/Game.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+
+//Singleton
+public class Game : MonoBehaviour
+{
+	private static Game gameInstance;
+
+	public static Game Instance
+	{
+		get
+		{
+			if (gameInstance == null)
+			{
+				var container = new GameObject("Game");
+				gameInstance = container.AddComponent<Game>();
+			}
+			return gameInstance;
+		}
+	}
+}
