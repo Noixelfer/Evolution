0076290313ec664b79b264763b97fb11d0c3400b
Noixelfer <pauldarius98@gmail.com>
2019-04-04 14:53:39 +0300

    Add PrefabManager class. This class will cache any dynamically instantiated prefab and you are able to instantiate a prefab (from one of the prefabPaths folders) using the prefab id(name)

---
+++ Evolution/Assets/Scripts/Core/PrefabsManager.cs
@@ -0,0 +1,66 @@
+﻿using Evolution.Path;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.IO;
+using System;
+using System.Linq;
+
+namespace Evolution
+{
+	public class PrefabsManager 
+	{
+		[Serializable]
+		private class PathDataList
+		{
+			public List<PathData> pathsData;
+		}
+
+		[Serializable]
+		private class PathData
+		{
+			public string name;
+			public string path;
+		}
+
+		private Dictionary<string, GameObject> prefabs = new Dictionary<string, GameObject>();
+		private List<PathData> paths = new List<PathData>();
+
+		public PrefabsManager()
+		{
+			//Load all paths from paths.json
+			string filePath = System.IO.Path.Combine(Application.streamingAssetsPath, "prefabPaths.json");
+			if (File.Exists(filePath))
+			{
+				string dataAsJson = File.ReadAllText(filePath);
+				var data = JsonUtility.FromJson<PathDataList>(dataAsJson);
+				paths.AddRange(data.pathsData);
+			}
+		}
+
+		public GameObject GetPrefab(string id)
+		{
+			if (prefabs.ContainsKey(id))
+				return prefabs[id];
+			else
+			{
+				if (paths.Any(pathData => GetPrefabForPathAndId(pathData.path, id) != null))
+					return prefabs[id];
+				else
+				{
+					Debug.LogError("There was no prefab with the id " + id + ". Maybe the path leading to that prefab is not saved in paths or there is no prefab with the given id");
+					return null;
+				}
+			}
+		}
+
+		private GameObject GetPrefabForPathAndId(string path, string id)
+		{
+			var prefab = Resources.Load(path + id) as GameObject;
+			if (prefab != null)
+				prefabs.Add(id, prefab);
+			return prefab;
+
+		}
+	}
+}
