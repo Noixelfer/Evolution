e31cce0cd2b88479dba3fb84095e4a605bdfdb3a
Noixelfer <pauldarius98@gmail.com>
2019-05-09 14:22:43 +0300

    Add Entity class, which implements IEntity interface

---
+++ Evolution/Assets/Scripts/Entities/Entity.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+using Evolution.Actions;
+
+namespace Evolution.Character
+{
+	public class Entity : IEntity
+	{
+		public string Name { get => throw new System.NotImplementedException(); set => throw new System.NotImplementedException(); }
+		public int Age { get => throw new System.NotImplementedException(); set => throw new System.NotImplementedException(); }
+		public List<IAction> KnownActions { get => throw new System.NotImplementedException(); set => throw new System.NotImplementedException(); }
+	}
+}
---
+++ Evolution/Assets/Scripts/Entities/IEntity.cs
@@ -0,0 +1,12 @@
+﻿using Evolution.Actions;
+using System.Collections.Generic;
+
+namespace Evolution.Character
+{
+	public interface IEntity
+	{
+		string Name { get; set; }
+		int Age { get; set; }
+		List<IAction> KnownActions { get; set; }
+	}
+}
