70d8f24d9a0bf0e960b99735fec9aa889652f737
Noixelfer <pauldarius98@gmail.com>
2019-05-19 16:50:27 +0300

    Add ResourceTypeGenerator which will return a prefab name for a given resource randomly taken from that resource type based on values saved in a .json file

---
+++ Evolution/Assets/Scripts/Map/ResourceTypeGenerator.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using UnityEngine;
+
+public class ResourceTypeGenerator
+{
+	[Serializable]
+	private class ResourcesDataList
+	{
+		public List<ResourcesData> resourcesData;
+	}
+
+	[Serializable]
+	private class ResourcesData
+	{
+		public string name;
+		public int spawnChance;
+	}
+
+	private Dictionary<string, ResourcesDataList> cachedResources;
+	private const string RESOURCE_DATA_FOLDER = "/ResourcesData/";
+
+	public ResourceTypeGenerator()
+	{
+		cachedResources = new Dictionary<string, ResourcesDataList>();
+	}
+
+	public string GetResourcePrefabName(string resourceName)
+	{
+		string resourcePrefabName = "";
+		if (cachedResources.ContainsKey(resourceName))
+		{
+			resourcePrefabName = GetResourceNameFromSpawnChance(cachedResources[resourceName]);
+		}
+		else
+		{
+			string filePath = Application.streamingAssetsPath + RESOURCE_DATA_FOLDER + resourceName;
+			if (File.Exists(filePath))
+			{
+				string dataAsJson = File.ReadAllText(filePath);
+				var data = JsonUtility.FromJson<ResourcesDataList>(dataAsJson);
+				if (data != null)
+				{
+					cachedResources.Add(resourceName, data);
+					resourcePrefabName = GetResourceNameFromSpawnChance(data);
+				}
+				else
+				{
+					Debug.LogError("Could not read json from " + filePath);
+				}
+			}
+		}
+		return resourcePrefabName;
+	}
+
+	/// <summary>
+	/// Returns the name of the resource tile after getting it randomly from spawn chance
+	/// </summary>
+	/// <param name="resourcesDataList">The list with resources and spawn chances</param>
+	/// <returns></returns>
+	private string GetResourceNameFromSpawnChance(ResourcesDataList resourcesDataList)
+	{
+		if (resourcesDataList == null)
+		{
+			Debug.LogError("Tried to get resource from null ResourcesDataList!!!");
+			return "";
+		}
+		int maxRandomNumber = 0;
+		foreach (var resource in resourcesDataList.resourcesData)
+		{
+			maxRandomNumber += resource.spawnChance;
+		}
+		int randomNumber = UnityEngine.Random.Range(0, maxRandomNumber);
+		int currentNumber = 0;
+		for (int i = 0; i < resourcesDataList.resourcesData.Count; i++)
+		{
+			var currentResource = resourcesDataList.resourcesData[i];
+			currentNumber += currentResource.spawnChance;
+			if (currentNumber >= randomNumber)
+				return currentResource.name;
+		}
+		return "";
+	}
+
+}
