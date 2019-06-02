e952e644b33d8c97ba44c7db819f34a31678f189
Noixelfer <pauldarius98@gmail.com>
2019-06-02 19:16:34 +0300

    Add IInteractable interface and InteractablesManager

---
+++ Evolution/Assets/Scripts/Core/IInteractable.cs
@@ -0,0 +1,16 @@
+﻿using Evolution.Actions;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Evolution
+{
+	/// <summary>
+	/// Interface implemented by all the object which can interract with our Agent
+	/// </summary>
+	public interface IInteractable
+	{
+		string ID { get; }
+		GameObject gameObject { get; }
+		List<IAction> GetPossibleActions();
+	}
+}
---
+++ Evolution/Assets/Scripts/Core/InteractablesManager.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+namespace Evolution
+{
+	public class InteractablesManager : MonoBehaviour
+	{
+		private HashSet<IInteractable> interactables = new HashSet<IInteractable>();
+
+		public void Register(IInteractable interactable)
+		{
+			interactables.Add(interactable);
+		}
+
+		public void Unregister(IInteractable interactable)
+		{
+			interactables.Remove(interactable);
+		}
+		
+		/// <summary>
+		/// Returns a HashSet with the IInteractables in the given radius from center
+		/// </summary>
+		/// <param name="position"></param>
+		/// <param name="radius"></param>
+		/// <returns></returns>
+		public HashSet<IInteractable> GetInteractablesAround(Vector3 position, float radius)
+		{
+			var squaredRadius = radius * radius;
+			var result = new HashSet<IInteractable>();
+			foreach (var interactable in interactables.ToList())
+			{
+				var squaredDistance = (position - interactable.gameObject.transform.position).sqrMagnitude;
+				if (squaredDistance <= squaredRadius)
+				{
+					result.Add(interactable);
+				}
+			}
+			return result;
+		}
+	}
+}
