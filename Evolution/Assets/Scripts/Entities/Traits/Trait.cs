450fb3ec4000ebe536b8eb48faabac2a83ff1d18
Noixelfer <pauldarius98@gmail.com>
2019-06-06 22:20:02 +0300

    Add Trait class and Traits, a static class containing all possible traits. Also, each trait name is saved as a constant string in Traits

---
+++ Evolution/Assets/Scripts/Entities/Traits/Trait.cs
@@ -0,0 +1,23 @@
+﻿namespace Evolution.Character
+{
+	[System.Serializable]
+	public class Trait
+	{
+		public Trait(string name, float percentage, string oppositeTrait)
+		{
+			Name = name;
+			OppositeTrait = oppositeTrait;
+			Percentage = percentage;
+		}
+
+		public Trait(string name, string oppositeTrait)
+		{
+			Name = name;
+			OppositeTrait = oppositeTrait;
+		}
+
+		public string Name;
+		public float Percentage = 1;
+		public string OppositeTrait;
+	}
+}
---
+++ Evolution/Assets/Scripts/Entities/Traits/Traits.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+namespace Evolution.Character
+{
+	public static class Traits
+	{
+		public static readonly string SOCIALBLE_TRAIT = "Sociable";
+		public static readonly string ACTIVE_TRAIT = "Active";
+		public static readonly string AGRESSIVE_TRAIT = "Agressive";
+		public static readonly string CAREFUL_TRAIT = "Careful";
+		public static readonly string LUST_TRAIT = "Lust";
+		public static readonly string GREEDY_TRADE = "Greedy";
+		public static readonly string FAST_LEARNER_TRAIT = "Fast Learner";
+
+		/// <summary>
+		/// List with all available traits
+		/// </summary>
+		public static readonly List<Trait> AllTraits = new List<Trait>()
+		{
+			new Trait(SOCIALBLE_TRAIT, "Atisocial"),
+			new Trait(ACTIVE_TRAIT, "Slauch"),
+			new Trait(AGRESSIVE_TRAIT, "Passive"),
+			new Trait(CAREFUL_TRAIT, "Gambler"),
+			new Trait(LUST_TRAIT, "Chastity"),
+			new Trait(GREEDY_TRADE, "Generous"),
+			new Trait(FAST_LEARNER_TRAIT, "Slow Learner")
+		};
+
+		public static List<Trait> GetRandomTraits()
+		{
+			var traits = new List<Trait>();
+			foreach (var trait in AllTraits)
+			{
+				trait.Percentage = Random.Range(0f, 1f);
+				traits.Add(trait);
+			}
+
+			return traits;
+		}
+	}
+}
