01b802e6f1adcca882976388d4673c16086233f9
Noixelfer <pauldarius98@gmail.com>
2019-06-07 16:07:18 +0300

    Add AgentsManager, each agent will register to this class. AgentManager will assign every agent a unique id

---
+++ Evolution/Assets/Scripts/Agents/AgentsManager.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections.Generic;
+
+namespace Evolution.Character
+{
+	public class AgentsManager
+	{
+		private static int currentID = 0;
+		private Dictionary<int, IAgent> allAgents;
+		public Game Game => Game.Instance;
+
+		public AgentsManager()
+		{
+
+		}
+
+		public void Register(IAgent agent)
+		{
+			if (agent == null)
+				return;
+			if (agent.ID != -1 && allAgents.ContainsKey(agent.ID))
+				return;
+
+			agent.ID = currentID;
+			allAgents.Add(currentID, agent);
+			currentID++;
+		}
+	}
+}
