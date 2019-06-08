76e3fb0d9d14127ece1bf4409d178d6d115ca476
Noixelfer <pauldarius98@gmail.com>
2019-06-08 20:13:37 +0300

    Add RockResource, stones are finally on map

---
+++ Evolution/Assets/Scripts/Resources/RockResource.cs
@@ -0,0 +1,18 @@
+﻿using Evolution.Actions;
+using Evolution.Character;
+using System.Collections.Generic;
+
+namespace Evolution.Resourcess
+{
+	public class RockResource : NaturalResource
+	{
+		public string Id = "";
+		public override string ID => Id;
+
+		public override List<IAction> GetPossibleActions(Agent agent)
+		{
+			var harvestAction = new HarvestNaturalResource(agent, "Gathering stone", 8f);
+			return new List<IAction>() { harvestAction };
+		}
+	}
+}
