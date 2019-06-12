6db155a1e0493664d49e2a936cdce8e41b83c98e
Noixelfer <pauldarius98@gmail.com>
2019-06-13 02:03:48 +0300

    Add UIStatusAgent script which will display informations about our agent current actions

---
+++ Evolution/Assets/Scripts/Agents/UI/UIAgentStatus.cs
@@ -0,0 +1,21 @@
+﻿using TMPro;
+using UnityEngine;
+
+namespace Evolution.Character
+{
+	public class UIAgentStatus : MonoBehaviour
+	{
+		private TextMeshPro agentStatus;
+		// Start is called before the first frame update
+		private void Start()
+		{
+			agentStatus = GetComponent<TextMeshPro>();
+		}
+
+		public void SetStatus(string status)
+		{
+			if (agentStatus != null)
+				agentStatus.text = status;
+		}
+	}
+}
