a1c5fe3aca0bf61325747ad624da9a798ddbc986
Noixelfer <pauldarius98@gmail.com>
2019-06-13 10:40:59 +0300

    Implement UI logic so that we can display informations about an agent

---
+++ Evolution/Assets/Scripts/Agents/UI/UIAgentViewer.cs
@@ -0,0 +1,44 @@
+﻿using Evolution.Character;
+using TMPro;
+using UnityEngine;
+
+namespace Evolution.UI
+{
+	public class UIAgentViewer : MonoBehaviour
+	{
+		public TextMeshProUGUI Name;
+		public TextMeshProUGUI Age;
+		public GameObject Traits;
+
+		public void ShowAgent(Agent agent)
+		{
+			if (agent == null)
+			{
+				Debug.LogError("The agent was null!");
+				return;
+			}
+
+			Name.text = agent.Name;
+			Age.text = agent.StatsManager.Age.GetAge().Years.ToString() + " years old";
+
+			DisplayTraits(agent);
+		}
+
+		private void DisplayTraits(Agent agent)
+		{
+			foreach (Transform go in Traits.transform)
+			{
+				if (go.GetComponent<UITrait>() != null)
+					Destroy(go);
+			}
+
+			foreach (var trait in agent.CharacterTraits)
+			{
+				var uITrait = Instantiate(Game.Instance.PrefabsManager.GetPrefab<UITrait>("UITrait"));
+				uITrait.SetName(trait.Value.Name);
+				uITrait.SetPercentage(trait.Value.Percentage);
+				uITrait.transform.SetParent(Traits.transform);
+			}
+		}
+	}
+}
---
+++ Evolution/Assets/Scripts/Agents/UI/UITrait.cs
@@ -0,0 +1,29 @@
+﻿using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace Evolution.UI
+{
+	public class UITrait : MonoBehaviour
+	{
+		private TextMeshProUGUI traitName;
+		private Image fillImage;
+
+		private void Awake()
+		{
+			traitName = GetComponent<TextMeshProUGUI>();
+			fillImage = transform.Find("PercentageBar").GetComponent<Image>();
+
+		}
+		public void SetName(string name)
+		{
+			traitName.text = name;
+		}
+
+		public void SetPercentage(float percentage)
+		{
+			percentage = Mathf.Clamp(percentage, 0, 1);
+			fillImage.fillAmount = percentage;
+		}
+	}
+}
