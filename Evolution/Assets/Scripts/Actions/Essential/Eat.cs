fdf6c18b4431971661986ab9afce9f9632ec262b
Noixelfer <pauldarius98@gmail.com>
2019-06-13 08:33:41 +0300

    Add Essential Folder in Actions, containing Eat and Sleep actions

---
+++ Evolution/Assets/Scripts/Actions/Essential/Eat.cs
@@ -0,0 +1,13 @@
+﻿namespace Evolution.Actions
+{
+	public class Eat : BaseAction
+	{
+		public override string ID => "Eat";
+		private float actionDuration;
+
+		public Eat()
+		{
+		}
+
+	}
+}
---
+++ Evolution/Assets/Scripts/Actions/Essential/Sleep.cs
@@ -0,0 +1,26 @@
+﻿using Evolution.Character;
+using UnityEngine;
+
+namespace Evolution.Actions
+{
+	public class Sleep : BaseAction
+	{
+		public override string ID => "Sleep";
+		private Agent agent;
+
+		public Sleep(Agent agent)
+		{
+			this.agent = agent;
+		}
+
+		/// <summary>
+		/// f(x) = [(x^3 + x^(1/3)) + 0.6x]/2
+		/// </summary>
+		/// <returns></returns>
+		public override float GetScoreBasedOnTraits()
+		{
+			var x = agent.StatsManager.Energy.Percentage;
+			return (Mathf.Pow(x, 3) + Mathf.Pow(x, 0.33f) + 0.6f * x) / 2;
+		}
+	}
+}
