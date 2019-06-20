67a31fa88ebbeb963e75abfed16c9e5080ec4384
Noixelfer <pauldarius98@gmail.com>
2019-06-20 18:39:27 +0300

    Create BaseItem and BaseEdibleItem

---
+++ Evolution/Assets/Scripts/Items/BaseEdibleItem.cs
@@ -0,0 +1,15 @@
+﻿namespace Evolution.Items
+{
+	public class BaseEdibleItem : IItem
+	{
+		public BaseEdibleItem(ItemDefinition itemDefinition, float hungerRestored)
+		{
+			ItemDefinition = itemDefinition;
+			HungerRestored = hungerRestored;
+		}
+		public ItemDefinition ItemDefinition;
+		public float HungerRestored;
+		public string Icon => "";
+		public float Weight => ItemDefinition.Weight;
+	}
+}
---
+++ Evolution/Assets/Scripts/Items/BaseItem.cs
@@ -0,0 +1,13 @@
+﻿namespace Evolution.Items
+{
+	public class BaseItem : IItem
+	{
+		public BaseItem(ItemDefinition itemDefinition)
+		{
+			ItemDefinition = itemDefinition;
+		}
+		public ItemDefinition ItemDefinition;
+		public string Icon => "";
+		public float Weight => ItemDefinition.Weight;
+	}
+}
