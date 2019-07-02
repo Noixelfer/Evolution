8ec4c0871cd27b3871c76294cc5800879f7a67ab
Noixelfer <pauldarius98@gmail.com>
2019-07-02 12:07:00 +0300

    Add Fishing and MoveToRandomPosition actions

---
+++ Evolution/Assets/Scripts/Actions/FishingAction.cs
@@ -0,0 +1,17 @@
+﻿using Evolution.Character;
+
+namespace Evolution.Actions
+{
+	public class FishingAction : BaseAction
+	{
+		public override string ID => "Fishing";
+		private Agent agent;
+		private float duration;
+
+		public FishingAction(Agent agent, float duration)
+		{
+			this.agent = agent;
+			this.duration = duration;
+		}
+	}
+}
